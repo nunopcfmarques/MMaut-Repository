1b3e1a269331ca883543de29a6bb161e44cc2e78
nunopcfmarques <nuno.figueiredo.marques@tecnico.ulisboa.pt>
2023-03-03 21:36:23 +0000

    Added plot for errors.

--- Project1_Code.py
+++ Project1_Code.py
@@ -21,17 +21,19 @@ def get_low_rank(U: np.ndarray, S: np.ndarray, Vh: np.ndarray, k: int) -> np.nda
     return A_k
 
 
-def get_errors(A: np.ndarray, U: np.ndarray, S: np.ndarray, Vh: np.ndarray, norm: str) -> np.ndarray:
+def get_errors(A: np.ndarray, U: np.ndarray, S: np.ndarray, Vh: np.ndarray, norm: str) -> np.ndarray[np.ndarray, np.ndarray]:
 
     size = np.size(S)
 
-    errors = np.zeros(size)
+    # erros in array with two sub-arrays each of size k.
+    # the first list is the norm of the error
+    # the second is the sum of squared residuals
+    errors = np.zeros((2, size))
 
     for k in range(1, size + 1):
         A_err = A - get_low_rank(U, S, Vh, k)
-        errors[k - 1] = np.linalg.norm(
-            A_err, ord=norm) ** 2
-
-    print(errors)
+        errors[0][k - 1] = np.linalg.norm(
+            A_err, ord=norm)
+        errors[1][k - 1] = np.sum(np.square(A_err))
 
     return errors
